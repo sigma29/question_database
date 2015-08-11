a92613deed40f2f60b905eda70f6425a437bf402
leah & gina <leah&gina@gmail.com>
2015-08-11 11:38:56 -0400

    Create find_by_id for all model classes for tables.

--- questions_database.rb
+++ questions_database.rb
@@ -86,7 +86,45 @@ class Reply
 end
 
 class QuestionFollow
+  def self.find_by_id(id)
+    results = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        question_follows
+      WHERE
+        question_follows.id = (?)
+    SQL
+    results.map { |result| QuestionFollow.new(result) }
+  end
+
+  attr_accessor :id, :question_id, :user_id
+
+  def initialize(opts = {})
+    @id = opts[id]
+    @question_id = opts[question_id]
+    @user_id = opts[user_id]
+  end
 end
 
 class QuestionLike
+  def self.find_by_id(id)
+    results = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        question_likes
+      WHERE
+        question_likes.id = (?)
+    SQL
+    results.map { |result| QuestionLike.new(result) }
+  end
+
+  attr_accessor :id, :question_id, :user_id
+
+  def initialize(opts = {})
+    @id = opts[id]
+    @question_id = opts[question_id]
+    @user_id = opts[user_id]
+  end
 end
